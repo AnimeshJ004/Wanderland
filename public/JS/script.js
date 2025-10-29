09b15c17e8e726026980b2a05e018067a38b76f2
AnimeshJ004 <animeshj425@gmail.com>
2025-10-29 08:32:12 +0530

    changes some files

--- public/JS/script.js
+++ public/JS/script.js
@@ -17,3 +17,25 @@
     }, false)
   })
 })()
+
+// Password toggle functionality
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleButtons = document.querySelectorAll('.toggle-password');
+
+  toggleButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      const input = this.previousElementSibling; // Get the input field
+      const icon = this.querySelector('i');
+
+      if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+      } else {
+        input.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+      }
+    });
+  });
+});
